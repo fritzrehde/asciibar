52c8ebe193d819d8a79a78c293c4ea5655ac4ab0
Fritz Rehde <fritzrehde.work@gmail.com>
2023-02-06 18:23:38 +0100

    Improved error handling syntax with idiomatic anyhow usage

--- src/percentage.rs
+++ src/percentage.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, bail, Result};
+use anyhow::{anyhow, bail, Context, Result};
 
 // internally saved as value between 0.0 and 1.0
 pub struct Percentage(f64);
@@ -15,20 +15,19 @@ impl Percentage {
 		Ok(match (min, max) {
 			(Some(min), Some(max)) => {
 				Self::new(percentage, min, max)
-					.ok_or_else(|| anyhow!("The percentage must be in the range of the specified minimum and maximum values"))?
+					.context("The percentage must be in the range of the specified minimum and maximum values")?
 			},
 			(None, None) => {
 				Self::new(percentage, 0.0, 1.0)
 					.or_else(|| Self::new(percentage, 0.0, 100.0))
-					.ok_or_else(|| anyhow!("The percentage must be in the range of the specified minimum and maximum values"))?
+					.ok_or_else(|| anyhow!("The percentage must be either in the default ranges 0.0 to 1.0 or 0.0 to 100.0, or in a custom range"))?
 			}
-			_ => bail!("When using a custom percentage scale, both the minimum and maximum values must be provided"),
+			_ => bail!("When using a custom percentage scale/range, both the minimum and maximum values must be provided"),
 		})
 	}
 
 	pub fn split_value(&self, value: usize) -> (usize, usize, usize) {
-		// TODO: look into max and min values of floats and ints
-		// TODO: find std lib method that floors and casts to u32 at once, seems error-prone this way
+		// saturating cast: saturates out-of-range values and maps NaN to 0
 		let exact_range = value as f64 * self.0;
 		let range1 = exact_range.floor() as usize;
 		let range2 = (exact_range.fract() >= 0.5).into();
